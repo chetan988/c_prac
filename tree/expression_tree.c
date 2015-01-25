867d4014d0978088fcc5b372df2904492a11f073
chetan <chetan988@gmail.com>
2015-01-25 12:30:26 +0530

    all req files

---
+++ tree/expression_tree.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tree.h"
+#include "stack.h"
+#include<string.h>
+
+struct tree*  createExprTree(char [],int);
+void preorder(struct tree *);
+void postorder(struct tree *);
+void inorder(struct tree *);
+void main() {
+	char expr[30],len;
+	//get the postfix expression from the console
+	printf("enter postfix expression :");
+	gets(expr);
+	printf("the entered expression is : %s\n",expr);
+	printf("\ncreating a expression tree\n");
+	len = strlen(expr);
+	struct tree *root = createExprTree(expr,len);
+	printf("\nprinting in preorder:\n");
+	preorder(root);
+	printf("\nprinting in postorder:\n");
+	postorder(root);
+	printf("\nprinting in  inorder:\n");
+	inorder(root);
+	printf("\n");
+}
+
+struct tree * createExprTree(char expr[],int len) {
+	int i;
+	struct stack *s = createStack(len);
+	struct tree *temp,*t2,*t1;
+	for(i=0;i<len;i++) {
+		temp=(struct tree *) malloc(sizeof(struct tree));
+		temp->data = expr[i];
+		if((expr[i] > 64 && expr[i] < 91) || (expr[i] > 96 && expr[i] <123)) {
+			// it is just a operand so push it
+			temp->left=NULL;
+			temp->right=NULL;
+		}
+		if(expr[i] == '*' || expr[i] == '+' || expr[i] == '-' || expr[i] == '/' ) {
+				t2=pop(s);			// get second operand
+				t1=pop(s);			// get first operand
+				temp->left=t1;		//put first in left
+				temp->right=t2;	// put second in right
+		}
+		push(s,temp);
+	}
+	return temp;
+}
+
+void preorder(struct tree *root) {
+	if(root == NULL)
+		return;
+	 printf("%c ",root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+void postorder(struct tree *root) {
+	if(root == NULL)
+		return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%c ",root->data);
+}
+void inorder(struct tree *root) {
+	if(root == NULL)
+       return;
+    inorder(root->left);
+    printf("%c ",root->data);
+    inorder(root->right);
+}
---
+++ tree/tree.h
@@ -0,0 +1,91 @@
+#ifndef tree_h
+
+#define tree_h
+
+struct tree {
+   int data;
+   struct tree *left;
+   struct tree *right;
+};
+
+// another tree have data,left,right,sibling
+
+struct tree_with_sibling {
+	int data;
+	struct tree_with_sibling *left;
+	struct tree_with_sibling *right;
+	struct tree_with_sibling *sibling;
+	
+};
+
+
+struct tree * createTreeFromArrayMid(int *,int,int);
+// recursive printing tree
+void r_preorder(struct tree *);
+void r_postorder(struct tree *);
+void r_inorder(struct tree *);
+
+// iterative printing tree
+void i_preoder(struct tree *);
+void i_inorder(struct tree *);
+void i_postorder(struct tree *);
+
+// level order traversal
+void levelorder(struct tree *,int,int);
+//get number of nodes in the tree
+int get_number_of_node(struct tree *);
+int get_height_of_tree(struct tree *);
+int i_get_height_of_tree_and_levelorder(struct tree *);
+
+// now find the maximum in the tree
+int findmax(struct tree *);
+
+// find a elememnt in the tree
+
+int findelement(struct tree *, int val);
+
+// find number of half nodes
+
+int findHalfNodes( struct tree *);
+
+// are two tree structurally same?
+
+int areTreeConfigSame(struct tree *, struct tree *);
+
+// are two tree same?
+
+int areTreeSame(struct tree *, struct tree *);
+
+//print root to leaf
+
+void printRootToLeaf(struct tree *, int *,int);
+
+// sum of all element in the tree
+
+int sumOfAllEle(struct tree *);
+
+// mirror the tree
+
+void mirror(struct tree *);
+
+// check if both tree are mirror with each other?
+int checkIfMirror(struct tree *, struct tree *);
+
+// vertical traverse
+
+void verticalTraverse(struct tree *);
+
+
+// create a tree with sibling
+// without recursion
+struct tree_with_sibling * createTreeFromArrayMid_ws(int *, int, int);
+void display_tree_with_sibling(struct tree_with_sibling *);
+void createTreeWithSibling(struct tree_with_sibling *);
+
+// check if tree is binary search tree or not
+
+int isBST(struct tree *);
+int isBST_1(struct tree *);
+int isBST_2(struct tree *);
+int isBST_3(struct tree *);
+#endif
